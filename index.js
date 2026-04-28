0d54d7657b340b91a75008b40298bf4e12c1e4ba
Jacqueline <al22020408@itsa.edu.mx>
2026-04-27 18:10:44 -0600

    imagenes y cambios de peliculas

--- index.js
+++ index.js
@@ -14,47 +14,47 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 let movies = [
   {
     id: 1,
-    title: "Mario",
+    title: "Barbie, Escuela de Princesas",
     duration: "190 min",
-    classification: "B",
-    genre: "Ciencia Ficción",
-    image: "https://apirest-y0s6.onrender.com/images/mario.jpg",
+    classification: "A",
+    genre: "Animada. Fantasia",
+    image: "https://apirest-y0s6.onrender.com/images/barbie.jpeg",
     showtimes: ["14:00", "18:00", "21:30"]
   },
   {
     id: 2,
-    title: "Predator",
-    duration: "175 min",
-    classification: "B15",
-    genre: "Acción",
-    image: "https://apirest-y0s6.onrender.com/images/predator.jpg",
+    title: "Cuando Harry encontro a Sally",
+    duration: "96 min",
+    classification: "A",
+    genre: "Romance. Comedia",
+    image: "https://apirest-y0s6.onrender.com/images/sally.jpeg",
     showtimes: ["13:00", "16:30", "20:00"]
   },
   {
     id: 3,
-    title: "Avispon",
-    duration: "92 min",
+    title: "Señales de amor",
+    duration: "90 min",
     classification: "A",
-    genre: "Acción",
-    image: "https://apirest-y0s6.onrender.com/images/avispon.jpg",
+    genre: "Comedia. Romance",
+    image: "https://apirest-y0s6.onrender.com/images/serende.jpeg",
     showtimes: ["11:00", "13:00", "15:00"]
   },
   {
     id: 4,
-    title: "Rango",
-    duration: "180 min",
+    title: "Siempre el Mismo Día",
+    duration: "110 min",
     classification: "B",
-    genre: "Comedia",
-    image: "https://apirest-y0s6.onrender.com/images/rango.jpg",
+    genre: "Comedia. Romance",
+    image: "https://apirest-y0s6.onrender.com/images/day.jpeg",
     showtimes: ["15:00", "19:00"]
   },
   {
     id: 5,
-    title: "Warfare",
-    duration: "114 min",
-    classification: "C",
-    genre: "Drama",
-    image: "https://apirest-y0s6.onrender.com/images/warfare.jpg",
+    title: "27 Bodas",
+    duration: "105 min",
+    classification: "B",
+    genre: "Drama. Romance. Comedia",
+    image: "https://apirest-y0s6.onrender.com/images/27.jpeg",
     showtimes: ["12:00", "14:30", "17:00"]
   }
 ];
