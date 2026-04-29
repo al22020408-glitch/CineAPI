637e84094262a385eeeda9750a5114db08306e91
Jacqueline <al22020408@itsa.edu.mx>
2026-04-29 08:03:11 -0600

    ajuste de imagenes

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ let movies = [
     duration: "190 min",
     classification: "A",
     genre: "Animada. Fantasia",
-    image: "https://apirest-y0s6.onrender.com/images/barbie.jpeg",
+    image: "https://cineapi-m7vu.onrender.com/movies/images/Barbie.png",
     showtimes: ["14:00", "18:00", "21:30"]
   },
   {
@@ -27,7 +27,7 @@ let movies = [
     duration: "96 min",
     classification: "A",
     genre: "Romance. Comedia",
-    image: "https://apirest-y0s6.onrender.com/images/sally.jpeg",
+    image: "https://cineapi-m7vu.onrender.com/movies/images/Sally.png",
     showtimes: ["13:00", "16:30", "20:00"]
   },
   {
@@ -36,7 +36,7 @@ let movies = [
     duration: "90 min",
     classification: "A",
     genre: "Comedia. Romance",
-    image: "https://apirest-y0s6.onrender.com/images/serende.jpeg",
+    image: "https://cineapi-m7vu.onrender.com/movies/images/Serende.png",
     showtimes: ["11:00", "13:00", "15:00"]
   },
   {
@@ -45,7 +45,7 @@ let movies = [
     duration: "110 min",
     classification: "B",
     genre: "Comedia. Romance",
-    image: "https://apirest-y0s6.onrender.com/images/day.jpeg",
+    image: "https://cineapi-m7vu.onrender.com/movies/images/Day.png",
     showtimes: ["15:00", "19:00"]
   },
   {
@@ -54,7 +54,7 @@ let movies = [
     duration: "105 min",
     classification: "B",
     genre: "Drama. Romance. Comedia",
-    image: "https://apirest-y0s6.onrender.com/images/27.jpeg",
+    image: "https://cineapi-m7vu.onrender.com/movies/images/27.png",
     showtimes: ["12:00", "14:30", "17:00"]
   }
 ];
